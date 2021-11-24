c1a9ca587dcf6903e1aa8ca890b53155a1cc2d58
MEDIDA NANDAKISHORE <94270473+medidanandakishore@users.noreply.github.com>
2021-11-24 07:44:01 +0530

    Add files via upload

---
+++ 3_Implementation/src/actvity1.c
@@ -0,0 +1,48 @@
+/**
+ * @file actvity1.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct library
+{
+char bk_name[30];
+char author[30];
+int pages;
+float price;
+};
+
+int main()
+{
+struct library l[100];
+char ar_nm[30],bk_nm[30];
+int i,j, keepcount;
+i=j=keepcount = 0;
+/**
+ * @brief 
+ * 
+ */
+while(j!=6)
+{
+printf("\n\n1. Add book information\n2. Display book information\n");
+printf("3. List all books of given author\n");
+printf("4. List the title of specified book\n");
+printf("5. List the count of books in the library\n");
+printf("6. Exit");
+
+printf ("\n\nEnter one of the above : ");
+scanf("%d",&j);
+
+switch (j)
+{
