61beaaf86a396ecc4a94cde2d3ab4dea270f0ce5
MEDIDA NANDAKISHORE <94270473+medidanandakishore@users.noreply.github.com>
2021-11-24 16:08:39 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,1 +1,27 @@
 
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+/**
+ * @brief 
+ * 
+ */
+struct library
+{
+char bk_name[30];
+char author[30];
+int pages;
+float price;
+};
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int main()
+{
+struct library l[100];
+char ar_nm[30],bk_nm[30];
+int i,j, keepcount;
+i=j=keepcount = 0;
