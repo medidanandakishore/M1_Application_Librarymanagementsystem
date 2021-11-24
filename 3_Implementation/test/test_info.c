8c9738ac8e4a7e93a2822cd6a5b4478390b784a7
MEDIDA NANDAKISHORE <94270473+medidanandakishore@users.noreply.github.com>
2021-11-24 08:09:22 +0530

    Add files via upload

---
+++ 3_Implementation/test/test_info.c
@@ -0,0 +1,120 @@
+/**
+ * @file test_info.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+/**
+ * @brief 
+ * 
+ */
+struct library
+{
+char bk_name[30];
+char author[30];
+int pages;
+float price;
+};
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int main()
+{
+struct library l[100];
+char ar_nm[30],bk_nm[30];
+int i,j, keepcount;
+i=j=keepcount = 0;
+while(j!=6)
+{
+printf("\n\n1. Add book information\n2. Display book information\n");
+printf("3. List all books of given author\n");
+printf("4. List the title of specified book\n");
+printf("5. List the count of books in the library\n");
+printf("6. Exit");
+
+printf ("\n\nEnter one of the above : ");
+scanf("%d",&j);
+/**
+ * @brief Construct a new switch object
+ * 
+ */
+switch (j)
+{
+/* Add book */
+case 1:  
+
+printf ("Enter book name = ");
+scanf ("%s",l[i].bk_name);
+
+printf ("Enter author name = ");
+scanf ("%s",l[i].author);
+
+printf ("Enter pages = ");
+scanf ("%d",&l[i].pages);
+
+printf ("Enter price = ");
+scanf ("%f",&l[i].price);
+keepcount++;
+
+break;
+/*enter the information*/
+
+case 2:
+printf("you have entered the following information\n");
+/**
+ * @brief 
+ * 
+ */
+for(i=0; i<keepcount; i++)
+{
+printf ("book name = %s",l[i].bk_name);
+
+printf ("\t author name = %s",l[i].author);
+
+printf ("\t  pages = %d",l[i].pages);
+
+printf ("\t  price = %f",l[i].price);
+}
+break;
+/* enter the author name*/
+case 3:
+printf ("Enter author name : ");
+scanf ("%s",ar_nm);
+for (i=0; i<keepcount; i++)
+{
+if (strcmp(ar_nm, l[i].author) == 0)
+printf ("%s %s %d %f",l[i].bk_name,l[i].author,l[i].pages,l[i].price);
+}
+break;
+/* enter book name*/
+case 4:
+printf ("Enter book name : ");
+scanf ("%s",bk_nm);
+for (i=0; i<keepcount; i++)
+{
+if (strcmp(bk_nm, l[i].bk_name) == 0)
+printf ("%s \t %s \t %d \t %f",l[i].bk_name,l[i].author,l[i].pages,l[i].price);
+}
+break;
+/* display the no of books in library */
+case 5:
+printf("\n No of books in library : %d", keepcount);
+break;
+case 6:
+exit (0); 
+
+}
+}
+return 0;
+
+}
